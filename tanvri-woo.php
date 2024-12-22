790e460068ab1f0bf051775495a6bb70db20aebd
Tanvir <tanvirlaravel@gmail.com>
2024-12-22 12:26:48 +0600

    woocommerce_before_add_to_cart_quantity

--- tanvri-woo.php
+++ tanvri-woo.php
@@ -21,31 +21,9 @@ if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get
     return;
 }
 
-add_action('woocommerce_after_single_product', 'wc_custom_before_single_product');
-function wc_custom_before_single_product(){
-    // is_product() is a conditional tag in WooCommerce that checks if the current page is a single product page. 
-   if(!is_product()){
-    return;
-   }
+add_action('woocommerce_before_add_to_cart_quantity', 'func_to_run');
 
-   // Display a custom banner
-   echo '
-   <div class="wc-custom-banner" style="background-color: #e6f7ff; padding: 20px; border: 2px solid #0073aa; margin-bottom: 20px; text-align: center;">
-        <h2>Special Offer!</h2>
-        <p>Free shipping on all orders over $50.</p>
-     </div>
-    ';
+function func_to_run() {
+    echo 'hi tanvir Im before add_to_cart';
 }
 
-add_action('wp_enqueue_scripts', 'wc_enqueue_custom_styles');
-function wc_enqueue_custom_styles() {
-    if (is_product()) {
-        wp_add_inline_style('woocommerce-general', '
-            .wc-custom-banner {
-                font-family: Arial, sans-serif;
-                color: #333;
-                border-radius: 5px;
-            }
-        ');
-    }
-}
