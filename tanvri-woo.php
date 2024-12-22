7a6076498a22c203822942ec264ba18f3c2f6b20
Tanvir <tanvirlaravel@gmail.com>
2024-12-22 13:27:16 +0600

    woocommerce_shop_loop_item_title

--- tanvri-woo.php
+++ tanvri-woo.php
@@ -21,18 +21,9 @@ if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get
     return;
 }
 
-add_action('woocommerce_archive_description', 'func_to_run');
+add_action('woocommerce_shop_loop_item_title', 'func_to_run');
 
 function func_to_run() {
-    if (is_product_category()) {
-        $category = get_queried_object(); // Get the current category object
-        $category_name = $category->name;
-
-        // Add custom message for specific categories
-        if ($category_name == 'Music') {
-            echo '<p style="color: green; font-weight: bold;">Exclusive deals on Music! Don\'t miss out on our amazing offers.</p>';
-        } elseif ($category_name == 'Clothing') {
-            echo '<p style="color: blue;">Free shipping on all clothing orders over $100. Shop now!</p>';
-        }
-    }
+    global $product;
+   echo $product->get_sku();
 }
